2791ef476e6f8fc06db4f10b0e2e7606cb7caeca
h181226 <simengjerde@hotmail.com>
2019-03-01 14:19:56 +0100

    PublishMsg ferdig

--- project2/src/no/hvl/dat110/messages/PublishMsg.java
+++ project2/src/no/hvl/dat110/messages/PublishMsg.java
@@ -2,7 +2,23 @@ package no.hvl.dat110.messages;
 
 public class PublishMsg extends Message {
 	
-	// TODO: 
-	// Implement objectvariables, constructor, get/set-methods, and toString method
+	private String topic;
 	
+	public PublishMsg(String user, String topic) {
+		super(MessageType.PUBLISH, user);
+		this.topic = topic;
+	}
+
+	public String getTopic() {
+		return topic;
+	}
+
+	public void setTopic(String topic) {
+		this.topic = topic;
+	}
+
+	@Override
+	public String toString() {
+		return "PublishMsg [topic=" + topic + ", Message" + super.toString() + "]";
+	}
 }

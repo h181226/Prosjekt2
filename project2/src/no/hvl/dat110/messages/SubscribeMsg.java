326895ecd12ef4ff26613581d121f32fa9213012
h181226 <simengjerde@hotmail.com>
2019-03-01 14:08:47 +0100

    SubscribeMsg ferdig

--- project2/src/no/hvl/dat110/messages/SubscribeMsg.java
+++ project2/src/no/hvl/dat110/messages/SubscribeMsg.java
@@ -1,6 +1,28 @@
 package no.hvl.dat110.messages;
 
 public class SubscribeMsg extends Message {
+	
+	private String topic;
+	
+	public SubscribeMsg(String user, String topic) {
+		super(MessageType.SUBSCRIBE, user);
+		this.topic = topic;
+	}
+
+	public String getTopic() {
+		return topic;
+	}
+
+	public void setTopic(String topic) {
+		this.topic = topic;
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + "\n" + "SubscribeMsg [topic=" + topic + "]";
+	}
+	
+	
 
 	// TODO: 
 	// Implement objectvariables, constructor, get/set-methods, and toString method

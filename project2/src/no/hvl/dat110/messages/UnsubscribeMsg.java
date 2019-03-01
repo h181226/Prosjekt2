31d8a9ea5a988cc29c59607b2b8dae868c50852b
h181226 <simengjerde@hotmail.com>
2019-03-01 14:16:24 +0100

    UnsubscribeMsg ferdig

--- project2/src/no/hvl/dat110/messages/UnsubscribeMsg.java
+++ project2/src/no/hvl/dat110/messages/UnsubscribeMsg.java
@@ -1,8 +1,24 @@
 package no.hvl.dat110.messages;
 
 public class UnsubscribeMsg extends Message {
-
-	// TODO: 
-	// Implement objectvariables, constructor, get/set-methods, and toString method
 	
+	private String topic;
+	
+	public UnsubscribeMsg(String user, String topic) {
+		super(MessageType.UNSUBSCRIBE, user);
+		this.topic = topic;
+	}
+
+	public String getTopic() {
+		return topic;
+	}
+
+	public void setTopic(String topic) {
+		this.topic = topic;
+	}
+
+	@Override
+	public String toString() {
+		return "UnsubscribeMsg [topic=" + topic + ", Message" + super.toString() + "]";
+	}
 }
